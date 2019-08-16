d803abf9755a1f037555e3c1393484c3f2e48de8
Bruno Vieira Costa <brunovcosta@outlook.com>
2019-08-16 02:11:29 -0300

    kills

--- src/common/player.ts
+++ src/common/player.ts
@@ -1,24 +1,27 @@
 import Question from './question';
 
 export default class Player {
-	state: "ALIVE" | "DEAD" | "OUT"
-	score: number;
-	index: number;
-	question: Question;
-	killedBy: Player;
+	public state: "ALIVE" | "DEAD" | "OUT"
+	public score: number;
+	public index: number;
+	public question: Question;
+	public killedBy: Player;
+	public kills: number;
 
 	constructor() {
 		this.state = "ALIVE";
 		this.score = 0;
+		this.kills = 0;
 	}
 
 	public setIndex(index: number) {
 		this.index = index;
 	}
 
-	public kill(player: Player = null) {
+	public kill(killer: Player = null) {
 		this.state = "DEAD";
-		this.killedBy = player
+		this.killedBy = killer
+		killer.kills += 1;
 	}
 
 	public drop() {
